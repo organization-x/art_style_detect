a8378be9bb9fb716de4dbca9c4a4b88c4a68540c
AAWorks <axalonso12@gmail.com>
2021-09-07 20:25:44 -0400

    updated with dict comprehension on line 10

--- get_masterlist.py
+++ get_masterlist.py
@@ -7,8 +7,8 @@ class Masterlist:
         
         self.dirpaths = ["train", "valid", "test"]      #paths (names if same dir) of image folders
 
-        #self.filepaths = [k:v for k, v in self.dirpaths, []]
-        self.filepaths = {"train": lists[0], "valid": lists[1], "test": lists[2]}
+        self.filepaths = {self.dirpaths[i]:lists[i] for i in range(len(self.dirpaths))}
+        print(self.filepaths)
 
     def crt_list(self, dirpath):
         full_path = os.path.join(self.cwd, dirpath)
